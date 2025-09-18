d427e17e08691fc12dee01daea48ba3b7c3e1369
nguyen dang tuyen <tuyen373809@gmail.com>
2025-09-18 14:40:25 +0700

    Remove accidental Downloads folder

--- Downloads/ung_dung_giao_duc-main-4/lib/game/plant_care/plant_care_game.dart
+++
@@ -1,7 +0,0 @@
-import '../core/game.dart';
-
-class PlantCareGame implements Game {
-  @override
-  final int difficulty; // 1, 2, 3
-  PlantCareGame({required this.difficulty});
-}
--- Downloads/ung_dung_giao_duc-main-4/lib/game/traffic_safety/traffic_safety_game.dart
+++
@@ -1,7 +0,0 @@
-import '../core/game.dart';
-
-class TrafficSafetyGame implements Game {
-  @override
-  final int difficulty; // 1, 2, 3
-  TrafficSafetyGame({required this.difficulty});
-}
